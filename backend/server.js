42ea565bca324eaa285be8e51f0d3503c5371b78
Marwan Kishk <kishkmarwan49@gmail.com>
2026-05-20 15:40:53 +0300

    start with backend and create models

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,26 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
+const dotenv = require('dotenv');
+dotenv.config();
+
+const PORT = process.env.PORT || 5005;
+const NODE_ENV = process.env.NODE_ENV || 'development';
 
 const app = express();
+
 
 app.get('/', (req, res) => {
     res.send('API is running...');
     });
-
-const PORT = process.env.PORT || 5005;
-
-app.listen(PORT, console.log(`Server running on port ${PORT}`));
+async function start() {
+    try {
+        await connectDB();
+        app.listen(PORT, console.log(`Server running in ${NODE_ENV} on port ${PORT}`));
+    } catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
+    
+}
+start();
